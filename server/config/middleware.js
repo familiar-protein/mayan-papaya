6ea2f0b1ce54bb495ebbd31709e0c6755dbb114c
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-20 11:21:35 -0700

    Added allowed methods in CORS middleware

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -10,6 +10,7 @@ module.exports = function (app, express) {
 
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, OPTIONS, POST");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
